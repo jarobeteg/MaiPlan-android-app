9609dd1531a389203586fc1d8ef9254f3896ce92
jarobeteg <jaroslavtm@gmail.com>
2025-03-04 11:00:13 +0100

    deleted dead code

--- app/src/main/java/com/example/maiplan/components/AppComponents.kt
+++ app/src/main/java/com/example/maiplan/components/AppComponents.kt
@@ -4,12 +4,10 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -44,7 +42,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.maiplan.R
@@ -336,29 +333,6 @@ fun DatePickerDialogComponent(
                 dayContentColor = MaterialTheme.colorScheme.onPrimary,
                 dayInSelectionRangeContentColor = MaterialTheme.colorScheme.secondaryContainer
             )
-        )
-    }
-}
-
-@Composable
-fun MoreScreenButton(text: String, onClick: () -> Unit) {
-    val interactionSource = remember { MutableInteractionSource() }
-
-    Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .clickable(
-                interactionSource = interactionSource,
-                indication = null,
-                onClick = onClick
-            )
-            .padding(vertical = 8.dp)
-    ) {
-        Text(
-            text = text,
-            color = MaterialTheme.colorScheme.onBackground,
-            fontSize = 16.sp,
-            textAlign = TextAlign.Start
         )
     }
 }
