a3bc604539189d597b7f246563217f753fdcf2b8
jarobeteg <jaroslavtm@gmail.com>
2025-11-01 12:20:32 +0100

    local data source basics are coded

--- app/src/main/java/com/example/maiplan/repository/category/CategoryLocalDataSource.kt
+++ app/src/main/java/com/example/maiplan/repository/category/CategoryLocalDataSource.kt
@@ -3,6 +3,7 @@ package com.example.maiplan.repository.category
 import android.content.Context
 import com.example.maiplan.database.MaiPlanDatabase
 import com.example.maiplan.database.dao.CategoryDAO
+import com.example.maiplan.database.entities.CategoryEntity
 
 class CategoryLocalDataSource(private val context: Context) {
 
@@ -12,5 +13,21 @@ class CategoryLocalDataSource(private val context: Context) {
 
     private val categoryDao: CategoryDAO by lazy {
         database.categoryDAO()
+    }
+
+    suspend fun getPendingCategory(categoryId: Int): CategoryEntity? {
+        return categoryDao.getPendingCategory(categoryId)
+    }
+
+    suspend fun getCategory(categoryId: Int): CategoryEntity? {
+        return categoryDao.getCategory(categoryId)
+    }
+
+    suspend fun categoryUpsert(category: CategoryEntity){
+        categoryDao.categoryUpsert(category)
+    }
+
+    suspend fun deleteCategory(category: CategoryEntity): Int {
+        return categoryDao.deleteCategory(category)
     }
 }
