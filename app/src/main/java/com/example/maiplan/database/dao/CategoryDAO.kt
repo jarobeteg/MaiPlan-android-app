241d857c750641bd79fbf2dc8c4b339a1c1c90ba
jarobeteg <jaroslavtm@gmail.com>
2025-11-01 12:20:16 +0100

    category DAO and local data source basics are coded

--- app/src/main/java/com/example/maiplan/database/dao/CategoryDAO.kt
+++ app/src/main/java/com/example/maiplan/database/dao/CategoryDAO.kt
@@ -1,7 +1,22 @@
 package com.example.maiplan.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import com.example.maiplan.database.entities.CategoryEntity
 
 @Dao
 interface CategoryDAO {
+    @Query("SELECT * FROM category WHERE category_id = :categoryId AND sync_state != 0")
+    suspend fun getPendingCategory(categoryId: Int): CategoryEntity?
+
+    @Query("SELECT * FROM category WHERE category_id = :categoryId")
+    suspend fun getCategory(categoryId: Int): CategoryEntity?
+
+    @Upsert
+    suspend fun categoryUpsert(entity: CategoryEntity)
+
+    @Delete
+    suspend fun deleteCategory(entity: CategoryEntity): Int
 }
