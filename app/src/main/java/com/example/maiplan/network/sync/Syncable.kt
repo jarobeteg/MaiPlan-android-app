dfa5b2e925230d98986000a1b387c6fe7344d233
jarobeteg <jaroslavtm@gmail.com>
2025-11-02 14:18:12 +0100

    forgot to add SerializedName for the userId Syncable data classes

--- app/src/main/java/com/example/maiplan/network/sync/Syncable.kt
+++ app/src/main/java/com/example/maiplan/network/sync/Syncable.kt
@@ -1,16 +1,18 @@
 package com.example.maiplan.network.sync
 
+import com.google.gson.annotations.SerializedName
+
 interface Syncable {
     suspend fun sync()
 }
 
 data class SyncRequest<T>(
-    val userId: Int,
+    @SerializedName("user_id") val userId: Int,
     val changes: List<T>
 )
 
 data class SyncResponse<T>(
-    val userId: Int,
+    @SerializedName("user_id") val userId: Int,
     val acknowledged: List<T>,   // records that were acknowledged during sync
     val rejected: List<T>       // records that were rejected during sync
 )
